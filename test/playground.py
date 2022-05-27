0121a08093a7d15af4f017e8ced91cbf2de7df2e
Yannick Uhlmann <augustunderground@protonmail.com>
2022-05-27 17:48:51 +0200

    added random testing

--- test/playground.py
+++ test/playground.py
@@ -17,18 +17,19 @@ o,r,d,i = env.step(a)
 
 ## SINGLE GOAL ENV, circus native
 env = circus.make('circus:op2-xh035-geom-v0')
-her = TD3( "MultiInputPolicy" , env , replay_buffer_class = HerReplayBuffer , replay_buffer_kwargs = dict( n_sampled_goal = 4 , goal_selection_strategy = 'future' , online_sampling = True , max_episode_length = 10 , ) , verbose = 1 , )
+
+her = TD3( "MultiInputPolicy"
+         , env
+         , replay_buffer_class = HerReplayBuffer
+         , replay_buffer_kwargs = dict( n_sampled_goal = 4
+                                      , goal_selection_strategy = 'future'
+                                      , online_sampling = True
+                                      , max_episode_length = 10
+                                      , )
+         , verbose = 1
+         , )
+
 her.learn(total_timesteps=2, log_interval=1)
-#her = TD3( "MultiInputPolicy"
-#         , env
-#         , replay_buffer_class = HerReplayBuffer
-#         , replay_buffer_kwargs = dict( n_sampled_goal = 4
-#                                      , goal_selection_strategy = 'future'
-#                                      , online_sampling = True
-#                                      , max_episode_length = 10
-#                                      , )
-#         , verbose = 1
-#         , )
 
 ## SINGLE Non-GOAL ENV, circus native
 env  = circus.make('circus:op2-xh035-geom-v1')
@@ -36,6 +37,11 @@ ddpg = DDPG('MlpPolicy', env, verbose = 1)
 ddpg.learn(total_timesteps=2, log_interval=1)
 
 ## VECTOR Non-GOAL ENV, circus native
-envs = circus.make('circus:op2-xh035-geom-v1', n_envs=5)
+envs = circus.make('circus:op2-xh035-geom-v1', n_envs = 5)
 a2c  = A2C("MlpPolicy", envs, verbose=1)
 a2c.learn(total_timesteps=2, log_interval=1)
+
+## Random Test
+env     = circus.make('circus:op1-xh035-elec-v0', n_envs = 5)
+a       = np.vstack([env.action_space.sample()] * 5)
+o,r,d,i = env.step(a)
