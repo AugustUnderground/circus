3e43674001fd978894467ae1fec34b53e0466c51
Yannick Uhlmann <augustunderground@protonmail.com>
2022-05-27 18:57:44 +0200

    add entrypoint and fix requirements

--- setup.py
+++ setup.py
@@ -8,20 +8,22 @@ with open('README.md', 'r') as fh:
 with open('requirements.txt', 'r') as req:
     requirements = req.read().splitlines()
 
+scripts: [str] = [ f'carnival = {package_name}.__main__:carnival']
+
 setup( name                          = package_name
      , version                       = '0.0.1'
-     , author                        = 'FIXME'
-     , author_email                  = 'FIXME'
-     , description                   = 'FIXME'
+     , author                        = 'Yannick Uhlmann'
+     , author_email                  = 'augustunderground@getgoogleoff.me'
+     , description                   = 'Analog Circuit Sizing Gym Environment'
      , long_description              = long_description
      , long_description_content_type = 'text/markdown'
-     , url                           = 'FIXME'
+     , url                           = 'https://github.com/augustunderground/circus'
      , packages                      = find_packages()
      , classifiers                   = [ 'Development Status :: 2 :: Pre-Alpha'
                                        , 'Programming Language :: Python :: 3'
                                        , 'Operating System :: POSIX :: Linux' ]
-     , python_requires               = '>=3.8'
+     , python_requires               = '>=3.9'
      , install_requires              = requirements
-     #, entry_points                  = { 'console_scripts': [ 'FIXME' ]}
-     , package_data                  = { '': ['*.hy', '*.coco', '__pycache__/*']}
+     , entry_points                  = { 'console_scripts': scripts }
+     , package_data                  = { '': ['__pycache__/*']}
      , )
