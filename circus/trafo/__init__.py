fix: found scaling issue, causing very large currents

circus/trafo/__init__.py
@@ -33,7 +33,9 @@ def electric_unscaler(ace_id: str, ace_backend: str) -> Callable:
 
     def unscale(x: np.ndarray) -> np.ndarray:
         y_ = x_min + (((x + 1.0) / 2.0) * (x_max - x_min))
-        y  = (y_ * gm) + np.power(10.0, y_ * fm) + (y_ * im * 1e-6)
+        y  = ( (y_ * gm)
+             + np.power( 10.0, y_ * fm, where = (y_ * fm) > 0.0)
+             + (y_ * im * 1e-6) )
         return y
 
     return unscale

circus/util.py
@@ -110,7 +110,8 @@ def performance_scaler( ace_id: str, ace_backend: str, constraints: dict
                                ) + (a * ~log_msk)
         y_           = 2.0 * (l[:,scl_msk] - x_min) / (x_max - x_min) - 1.0
         y            = x.copy()
-        y[:,scl_msk] = np.clip(y_, -2.0, 2.0)
+        #y[:,scl_msk] = np.clip(y_, -2.0, 2.0)
+        y[:,scl_msk] = y_
         return y
 
     def unscaler(y: np.ndarray) -> np.ndarray: